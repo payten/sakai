d97874882616f1a182e9d13bca9555711f88de17
Payten Giles <payten.giles@gmail.com>
2015-05-12 13:37:25 +1000

    Add Banner Alert code and add API to return the active banners as a JSON string

---
+++ pasystem/pasystem-impl/impl/src/java/org/sakaiproject/pasystem/impl/banners/BannerAlert.java
@@ -0,0 +1,55 @@
+package org.sakaiproject.pasystem.impl.banners;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class BannerAlert {
+  public String uuid;
+  public String message;
+
+  private String hosts;
+  private boolean isActive;
+  private boolean isDismissible;
+  private long activeFrom;
+  private long activeUntil;
+
+  public BannerAlert(String uuid, String message, String hosts, int dismissible, int active, long activeFrom, long activeUntil) {
+    this.uuid = uuid;
+    this.message = message;
+    this.hosts = hosts;
+    this.isActive = (active == 1);
+    this.isDismissible = (dismissible == 1);
+    this.activeFrom = activeFrom;
+    this.activeUntil = activeUntil;
+  }
+
+  public boolean isActive() {
+    if (activeFrom == 0 && activeUntil == 0) {
+      return isActive;
+    }
+
+    Date now = new Date();
+
+    return (now.after(new Date(activeFrom))
+            && (activeUntil == 0 || now.before(new Date(activeUntil))));
+  }
+
+  public boolean isDismissible() {
+    return this.isDismissible;
+  }
+
+  public boolean isActiveForHost(String hostname) {
+    // are we active?
+    if (!isActive()) {
+      return false;
+    }
+
+    // if no hosts then assume active for any host
+    if (hosts == null || hosts.isEmpty()) {
+      return true;
+    }
+
+    // we have some hosts defined, so check if the current is listed
+    return Arrays.asList(hosts.split(",")).contains(hostname);
+  }
+}
