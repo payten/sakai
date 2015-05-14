9311f0f1ff4d8c250d09babdfc310fc8c3d53f8d
Mark Triggs <mark@dishevelled.net>
2015-05-14 19:57:36 +1000

    Pull out query handling too

---
+++ pasystem/pasystem-impl/impl/src/java/org/sakaiproject/pasystem/impl/common/DBResults.java
@@ -0,0 +1,49 @@
+package org.sakaiproject.pasystem.impl.common;
+
+import java.util.Iterator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBResults implements Iterable<ResultSet>, Iterator<ResultSet>, AutoCloseable {
+    private PreparedStatement originalStatement;
+    private ResultSet resultSet;
+    private boolean hasRowReady;
+
+    public DBResults(ResultSet rs, PreparedStatement originalStatement) {
+        this.resultSet = rs;
+        this.originalStatement = originalStatement;
+    }
+
+    public void close() throws SQLException {
+        resultSet.close();
+        originalStatement.close();
+    }
+
+    public boolean hasNext() {
+        try {
+        if (!hasRowReady) {
+            hasRowReady = resultSet.next();
+        }
+
+        return hasRowReady;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public ResultSet next() {
+        if (!hasRowReady) {
+            throw new RuntimeException("Read past end of results");
+        }
+
+        hasRowReady = false;
+        return resultSet;
+    }
+
+    public Iterator<ResultSet> iterator() {
+        return this;
+    }
+}
