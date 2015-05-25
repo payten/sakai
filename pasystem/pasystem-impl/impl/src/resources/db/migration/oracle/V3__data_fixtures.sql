86029916acb2e418b818d35cb266c0872951a98b
Mark Triggs <mark@dishevelled.net>
2015-05-25 16:25:23 +1000

    Oracle migration fix

--- pasystem/pasystem-impl/impl/src/resources/db/migration/oracle/V3__data_fixtures.sql
+++ pasystem/pasystem-impl/impl/src/resources/db/migration/oracle/V3__data_fixtures.sql
@@ -1,1 +1,15 @@
-INSERT IGNORE INTO SAKAI_REALM_FUNCTION (FUNCTION_KEY, FUNCTION_NAME) values (SAKAI_REALM_FUNCTION_SEQ.NEXTVAL, 'pasystem.manage');
+-- Magic '-123' value here never gets used: if the merge succeeds it
+-- takes NEXTVAL from the sequence.  If it fails (because the function
+-- is already defined), then nothing gets inserted.  We just include
+-- the placeholder to keep Oracle happy.
+-- 
+
+MERGE INTO SAKAI_REALM_FUNCTION srf
+USING (
+SELECT -123 as function_key, 
+'pasystem.manage' as function_name
+FROM dual
+) t on (srf.function_name = t.function_name) 
+WHEN NOT MATCHED THEN 
+INSERT (function_key, function_name)
+VALUES (SAKAI_REALM_FUNCTION_SEQ.NEXTVAL, t.function_name);
