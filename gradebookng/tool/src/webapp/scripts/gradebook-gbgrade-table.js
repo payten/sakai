fccbaaba58e7df36ba94ce85531884d9aee88a6e
Mark Triggs <mark@dishevelled.net>
2016-03-16 15:58:05 +1100

    Better formatting

--- gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
@@ -43,7 +43,7 @@ GbGradeTable.unpack = function (s, rowCount, columnCount) {
 };
 
 
-SAMPLE_CELL = '<div role="gridcell" tabindex="0" class="gb-grade-item-cell" data-assignmentid="%{ASSIGNMENT_ID}" data-studentuuid="%{STUDENT_UUID}" aria-readonly="false"><div><input type="text" tabindex="-1" class="gb-editable-grade" name="table:body:rows:1:cells:4:cell:editableGrade" id="editableGradef" value="%{GRADE}"><!-- dropdown menu --><div class="btn-group"> <a class="btn btn-sm btn-default dropdown-toggle" title="%{TITLE}" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" "> <span class="caret"></span> </a> <ul class="dropdown-menu dropdown-menu-right" role="menu"> <li><a href="javascript:;" role="menuitem" id="viewGradeLog11">Grade Log</a></li><li><a href="javascript:;" role="menuitem" class="gb-edit-comments" id="editGradeComment12"><span>Add Comment</span></a></li> </ul> </div></div></div>';
+SAMPLE_CELL = '<div role="gridcell" tabindex="0" class="gb-grade-item-cell" data-assignmentid="%{ASSIGNMENT_ID}" data-studentuuid="%{STUDENT_UUID}" aria-readonly="false"><div><input type="text" tabindex="-1" class="gb-editable-grade" id="editableGradef" value="%{GRADE}"><!-- dropdown menu --><div class="btn-group"> <a class="btn btn-sm btn-default dropdown-toggle" title="%{TITLE}" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" "> <span class="caret"></span> </a> <ul class="dropdown-menu dropdown-menu-right" role="menu"> <li><a href="javascript:;" role="menuitem" id="viewGradeLog11">Grade Log</a></li><li><a href="javascript:;" role="menuitem" class="gb-edit-comments" id="editGradeComment12"><span>Add Comment</span></a></li> </ul> </div></div></div>';
 
 GbGradeTable.cellRenderer = function (instance, td, row, col, prop, value, cellProperties) {
   var wasInitialised = td.getAttribute('data-cell-initialised');
@@ -52,12 +52,13 @@ GbGradeTable.cellRenderer = function (instance, td, row, col, prop, value, cellP
     td.setAttribute('data-cell-initialised', '1');
 
     var $td = $(td);
+
     // First time we've initialised this cell.
     var html = SAMPLE_CELL;
-    html = html.replace('%{ASSIGNMENT_ID}', '27');
-    html = html.replace('%{STUDENT_UUID}', 'dbf3ef87-4aa4-48cf-b900-3e7bcef79d8f');
+    html = html.replace('%{ASSIGNMENT_ID}', GbGradeTable.assignments[col]);
+    html = html.replace('%{STUDENT_UUID}', GbGradeTable.students[row]);
     html = html.replace('%{GRADE}', ('' + GbGradeTable.grades[row][col]));
-    html = html.replace('%{TITLE}', "Open menu for student (whoever) and assignment (whatever) cell");
+    html = html.replace('%{TITLE}', "Open menu for student " + GbGradeTable.students[row] + " and assignment " + GbGradeTable.assignments[col] + " cell");
 
     $td.html(html);
   }
@@ -65,18 +66,22 @@ GbGradeTable.cellRenderer = function (instance, td, row, col, prop, value, cellP
 
 // FIXME: Hard-coded stuff here
 GbGradeTable.renderTable = function (elementId, assignmentList, studentList, data) {
+
+  GbGradeTable.students = studentList;
+  GbGradeTable.assignments = assignmentList;
   GbGradeTable.grades = data;
+
   GbGradeTable.instance = new Handsontable(document.getElementById(elementId), {
     data: data,
     rowHeaders: studentList,
     rowHeaderWidth: 120,
-    colHeaders: assignmentList,
+    colHeaders: assignmentList.map(function (id) { return "Assignment #<" + id + ">"}),
     columns: assignmentList.map(function () { return {renderer: GbGradeTable.cellRenderer} }),
-    colWidths: assignmentList.map(function () { return 200 }),
+    colWidths: assignmentList.map(function () { return 230 }),
     autoRowSize: false,
     autoColSize: false,
     height: 600,
-    width: 1200,
+    width: $('#' + elementId).width(),
   });
 
 };
