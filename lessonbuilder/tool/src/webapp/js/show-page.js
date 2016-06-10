11abe2d2612fa5ee7997da1609500eddc7724c87
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:52:25 +0000

    CLASSES-1849 More spaghetti for show-page.js
    Avoid SCORM inputs from conflicting with non-SCORM types by disabling
    them selectively.

--- lessonbuilder/tool/src/webapp/js/show-page.js
+++ lessonbuilder/tool/src/webapp/js/show-page.js
@@ -1701,6 +1701,8 @@ $(document).ready(function() {
 			var editurl = row.find(".edit-url").text();
 			var editsettingsurl = row.find(".edit-settings-url").text();
 			
+                        $("#scormstuff :input").prop("disabled", type != "scorm")
+
 			if(type === 'page') {
 	                    $("#pagestuff").show();
 
