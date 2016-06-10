a8470c9361f8587525d15ee425abf9fd183f8a32
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 07:07:35 +0000

    CLASSES-2186 Fix support for groups in SCORM lessons
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324278 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
@@ -3306,6 +3306,7 @@ public class SimplePageBean {
 	       case SimplePageItem.COMMENTS:
 	       case SimplePageItem.QUESTION:
 	       case SimplePageItem.STUDENT_CONTENT:
+	       case SimplePageItem.SCORM:
 		   return getLBItemGroups(i); // for all native LB objects
 	       default:
 	    	   return null;
@@ -3491,6 +3492,7 @@ public class SimplePageBean {
 	   case SimplePageItem.COMMENTS:
 	   case SimplePageItem.QUESTION:
 	   case SimplePageItem.STUDENT_CONTENT:
+	   case SimplePageItem.SCORM:
 	       return setLBItemGroups(i, groups);
 	   case SimplePageItem.BREAK:
 	       return null;  // better not actually happen
