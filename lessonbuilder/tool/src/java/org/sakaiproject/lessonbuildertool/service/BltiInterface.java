70e1cd38722b9edab169e4f7e4efb9058a598355
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:51:15 +0000

    /CLASSES-1847 Expose whether blti tools have are available to a site on the lessons BltiEntity (this hits the LTIService) and use this value to detemermine if the Add External Tools action should be displayed for a particular site.
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324174 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/service/BltiInterface.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/service/BltiInterface.java
@@ -20,5 +20,7 @@ public interface BltiInterface {
     public boolean isPopUp();
     public int frameSize();
     public String doImportTool(String launchUrl, String bltiTitle, String strXml, String custom);
+    //CLASSES-1847 Expose whether blti tools have are available to a site
+    public boolean hasToolsAvailable();
 }
 
