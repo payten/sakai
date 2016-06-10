79ba4595aefa2e05635b30f1ea86841776635439
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 05:28:56 +0000

    CLASSES-1032 Incorporate the Longsight Web Services
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@323787 66ffb92e-73f9-0310-93c1-f5514f145a0a
    
    Conflicts:
    	webservices/cxf/pom.xml
    	webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
    	webservices/cxf/src/resources/applicationContext.xml

--- webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
+++ webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
@@ -48,6 +48,12 @@ import org.sakaiproject.shortenedurl.api.ShortenedUrlService;
 import org.sakaiproject.tool.assessment.samlite.api.SamLiteService;
 import org.sakaiproject.id.api.IdManager;
 import org.sakaiproject.tool.assessment.shared.api.questionpool.QuestionPoolServiceAPI;
+import org.sakaiproject.archive.api.ArchiveService;
+import org.sakaiproject.user.api.PreferencesService;
+import org.sakaiproject.memory.api.MemoryService;
+import org.sakaiproject.db.api.SqlService;
+import org.sakaiproject.importer.api.ImportService;
+import org.sakaiproject.api.app.syllabus.SyllabusManager;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -88,6 +94,12 @@ public class AbstractWebService {
     protected GradebookExternalAssessmentService gradebookExternalAssessmentService;
     protected ActivityService activityService;
     protected QuestionPoolServiceAPI questionPoolServiceImpl;
+    protected ArchiveService archiveService;
+    protected PreferencesService preferencesService;
+    protected MemoryService memoryService;
+    protected SqlService sqlService;
+    protected ImportService importService;
+    protected SyllabusManager syllabusManager;
 
     
     @WebMethod(exclude = true)
@@ -266,5 +278,34 @@ public class AbstractWebService {
     @WebMethod(exclude = true)
     public void setQuestionPoolServiceImpl(QuestionPoolServiceAPI questionPoolServiceImpl) {
         this.questionPoolServiceImpl = questionPoolServiceImpl;
+
+    @WebMethod(exclude = true)
+    public void setArchiveService(ArchiveService archiveService) {
+        this.archiveService = archiveService;
+    }
+    
+    @WebMethod(exclude = true)
+    public void setPreferencesService(PreferencesService preferencesService) {
+        this.preferencesService = preferencesService;
+    }
+    
+    @WebMethod(exclude = true)
+    public void setMemoryService(MemoryService memoryService) {
+        this.memoryService = memoryService;
+    }
+    
+    @WebMethod(exclude = true)
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
+    
+    @WebMethod(exclude = true)
+    public void setImportService(ImportService importService) {
+        this.importService = importService;
+    }
+    
+    @WebMethod(exclude = true)
+    public void setSyllabusManager(SyllabusManager syllabusManager) {
+        this.syllabusManager = syllabusManager;
     }
 }
