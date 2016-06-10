4a7b464e64e179c09c342c25948348a3bcecbe51
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:30:43 +0000

    SU-78 Show section ID on the site settings page.

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4801,6 +4801,10 @@ public class SiteAction extends PagedResourceActionII {
 				}
 			}
 			context.put("providerCourseTitles", sectionTitles);
+
+                        // CLASSES-455 NYU Change - sort course list by id
+                        Collections.sort(providerCourseList);
+
 			context.put("providerCourseList", providerCourseList);
 		}
 
