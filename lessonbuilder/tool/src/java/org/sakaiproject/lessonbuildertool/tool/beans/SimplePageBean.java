145c9499ed906df36364e2c2c04c2c029152d342
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:44:42 +0000

    CLASSES-1606 Change poll bar graph orientation to horizontal and include alpha-counter on answers
    CLASSES-1606 If a poll answer is marked as 'correct', then display this to the user after they have answered the question
    
    CLASSES-1606 Change wording of message displayed to instructors when previewing a lessons poll
    
    CLASSES-1606 Introduce new checkbox option for multiplechoice and shortanswer questions to show the correct answers to the student after response.  Also introduce a table view when creating a multiple choice question to help provide context for the checkbox (correct answer) and text field (answer text).
    
    CLASSES-1606 Show letters for the correct answers if question type is multiplechoice.  Continue to show the answer text if question type is shortanswer.
    
    CLASSES-1606 Bold the correct answer message

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
@@ -264,6 +264,7 @@ public class SimplePageBean {
     public String questionText, questionCorrectText, questionIncorrectText;
     public String questionAnswer;
     public Boolean questionShowPoll;
+    public Boolean questionShowCorrectAnswers; /* CLASSES-1606 */
     private HashMap<Integer, String> questionAnswers = null;
     
     public Long questionId;
@@ -7361,6 +7362,7 @@ public class SimplePageBean {
 		item.setAttribute("questionText", questionText);
 		item.setAttribute("questionCorrectText", questionCorrectText);
 		item.setAttribute("questionIncorrectText", questionIncorrectText);
+		item.setAttribute("questionShowCorrectAnswers", String.valueOf(questionShowCorrectAnswers)); /* CLASSES-1606 */
 		item.setAttribute("questionType", questionType);
 		
 		if(questionType.equals("shortanswer")) {
