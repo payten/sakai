816b31784b9d20f5dbb7d933a9fa5ad10373a16b
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 05:38:22 +0000

    CLASSES-1043 Port changes for CLASSES-522

--- announcement/announcement-tool/tool/src/java/org/sakaiproject/announcement/tool/AnnouncementAction.java
+++ announcement/announcement-tool/tool/src/java/org/sakaiproject/announcement/tool/AnnouncementAction.java
@@ -1526,8 +1526,10 @@ public class AnnouncementAction extends PagedResourceActionII
 					initMergeList = tc.getPlacementConfig().getProperty(PORTLET_CONFIG_PARM_MERGED_CHANNELS);	
 				}
 
-				if (isOnWorkspaceTab() && !m_securityService.isSuperUser())
+				if (isOnWorkspaceTab())
 				{
+					M_log.debug("Getting for My Workspace");
+
 					String[] channelArrayFromConfigParameterValuebeBefore = null;
 
 					channelArrayFromConfigParameterValuebeBefore = mergedAnnouncementList
@@ -1574,6 +1576,9 @@ public class AnnouncementAction extends PagedResourceActionII
 				}
 				else
 				{
+				
+					M_log.debug("Normal site, getting from configured merge list");
+				
 					channelArrayFromConfigParameterValue = mergedAnnouncementList
 					.getChannelReferenceArrayFromDelimitedString(state.getChannelId(), initMergeList);
 					
@@ -1604,6 +1609,7 @@ public class AnnouncementAction extends PagedResourceActionII
 			}
 		}
 
+		//NYU mod, ignore super user status, force to false. We want all merged messages
 		mergedAnnouncementList.loadChannelsFromDelimitedString(
 		        isOnWorkspaceTab(), 
 		        new MergedListEntryProviderFixedListWrapper(
@@ -1613,7 +1619,7 @@ public class AnnouncementAction extends PagedResourceActionII
 		            new AnnouncementReferenceToChannelConverter() ), 
 		        StringUtils.trimToEmpty(SessionManager.getCurrentSessionUserId()), 
 		        channelArrayFromConfigParameterValue, 
-		        m_securityService.isSuperUser(),
+				false,
 		        ToolManager.getCurrentPlacement().getContext());
 
 		Iterator channelsIt = mergedAnnouncementList.iterator();
