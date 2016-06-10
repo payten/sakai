e15beb33e027e394e54f363278ce104be3a59622
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 07:02:50 +0000

    CLASSES-2030 sort sections by eid alphabetically on the Edit Roster page (as per the info page)
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324259 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4860,6 +4860,9 @@ public class SiteAction extends PagedResourceActionII {
 				courseList.add(courseListString);
 			}
 			
+			// CLASSES-2030 Ensure course list is ordered alphabetically
+			Collections.sort(courseList);
+			
 			if (state_attribute_string.equals(STATE_CM_REQUESTED_SECTIONS))
 			{
 				// need to construct the list of SectionObjects
