c165b5f4e7d17e190b4870cd9a63fc00296d89bb
Mark Triggs <mark@dishevelled.net>
2016-11-07 14:37:58 +1100

    CLASSES-2095 Update webservices calls to findTools

--- webservices/cxf/src/java/org/sakaiproject/webservices/SakaiScript.java
+++ webservices/cxf/src/java/org/sakaiproject/webservices/SakaiScript.java
@@ -4690,7 +4690,7 @@ public class SakaiScript extends AbstractWebService {
             if (!securityService.isSuperUser(session.getUserId())) {
 
                 Set categories = new HashSet<String>();
-                Set<Tool> visibleTools = toolManager.findTools(categories, null);
+                Set<Tool> visibleTools = toolManager.findTools(categories, null, null);
 
                 boolean toolVisible = false;
                 for (Tool tool : visibleTools) {
@@ -4705,7 +4705,7 @@ public class SakaiScript extends AbstractWebService {
                 }
 
                 categories.add(siteEdit.getType());
-                Set<Tool> availableTools = toolManager.findTools(categories, null);
+                Set<Tool> availableTools = toolManager.findTools(categories, null, null);
 
                 boolean toolAvailable = false;
                 for (Tool tool : availableTools) {
