15ca5d1413f1ab66578610192026e780f996ee56
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-12-16 03:03:23 +0000

    CLASSES-1197 Return an empty list instead of throwing a 403 if the user doesn't have access
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@325597 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/ui/helpers/entity/GradebookEntityProvider.java
+++ gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/ui/helpers/entity/GradebookEntityProvider.java
@@ -153,10 +153,6 @@ public class GradebookEntityProvider extends AbstractEntityProvider implements
 		}
 		Site site = this.getSite(siteId);
 
-		if(!isToolAccessible(site)) {
-			throw new SecurityException("Gradebook is not accessible for site: "+ siteId);
-		}
-
 		// The gradebookUID is the siteId, the gradebookID is a long
 		if (!gradebookService.isGradebookDefined(siteId)) {
 			throw new IllegalArgumentException("No gradebook found for site: "
@@ -188,6 +184,11 @@ public class GradebookEntityProvider extends AbstractEntityProvider implements
 		} else {
 			// students or the rest
 			GradeCourse course = new GradeCourse(site);
+
+			if(!isToolAccessible(site)) {
+				// Don't give any details
+				return course;
+			}
 
 			List<Assignment> gbitems = gradebookService
 					.getViewableAssignmentsForCurrentUser(siteId);
