ed67fb097b8c55b307a1cf138459a5cc4505a13e
Mark Triggs <mark@dishevelled.net>
2017-05-25 14:34:31 +1000

    CLASSES-2798 Apply relevant parts of 85ca404dae2be0bb06d94701af44585d07e555b1

--- edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookServiceHibernateImpl.java
+++ edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookServiceHibernateImpl.java
@@ -397,9 +397,7 @@ public class GradebookServiceHibernateImpl extends BaseHibernateManager implemen
 		rval.setCourseLetterGradeDisplayed(gradebook.isCourseLetterGradeDisplayed());
 		rval.setCoursePointsDisplayed(gradebook.isCoursePointsDisplayed());
 		rval.setCourseAverageDisplayed(gradebook.isCourseAverageDisplayed());
-		
-		rval.setFinalGradeMode(gradebook.isFinalGradeMode());
-				
+						
 		return rval;
 	}
 	
@@ -3300,19 +3298,17 @@ public class GradebookServiceHibernateImpl extends BaseHibernateManager implemen
 		
 		//Before we do any work, check if any existing course grade overrides might be left in an unmappable state
 		// this is NOT done when in final grade mode as the course grade field is now arbitrary.
-		if(!gbInfo.isFinalGradeMode()) {
-			List<CourseGradeRecord> courseGradeOverrides = (List<CourseGradeRecord>)getHibernateTemplate().execute(new HibernateCallback() {
-	            @Override
-				public Object doInHibernate(Session session) throws HibernateException {
-	                return getCourseGradeOverrides(gradebook, session);
-	            }
-			});		
-			courseGradeOverrides.forEach(cgr -> {
-				if(!bottomPercents.containsKey(cgr.getEnteredGrade())) {
-					throw new UnmappableCourseGradeOverrideException("The grading schema could not be updated as it would leave some course grade overrides in an unmappable state.");
-				}
-			});
-		}
+		List<CourseGradeRecord> courseGradeOverrides = (List<CourseGradeRecord>)getHibernateTemplate().execute(new HibernateCallback() {
+			@Override
+			public Object doInHibernate(Session session) throws HibernateException {
+				return getCourseGradeOverrides(gradebook, session);
+			}
+		});		
+		courseGradeOverrides.forEach(cgr -> {
+					if(!bottomPercents.containsKey(cgr.getEnteredGrade())) {
+						throw new UnmappableCourseGradeOverrideException("The grading schema could not be updated as it would leave some course grade overrides in an unmappable state.");
+					}
+				});
 		
 		//iterate all available grademappings for this gradebook and set the one that we have the ID and bottomPercents for
 		Set<GradeMapping> gradeMappings = gradebook.getGradeMappings();
@@ -3339,9 +3335,7 @@ public class GradebookServiceHibernateImpl extends BaseHibernateManager implemen
 		gradebook.setCourseLetterGradeDisplayed(gbInfo.isCourseLetterGradeDisplayed());
 		gradebook.setCoursePointsDisplayed(gbInfo.isCoursePointsDisplayed());
 		gradebook.setCourseAverageDisplayed(gbInfo.isCourseAverageDisplayed());
-		
-		gradebook.setFinalGradeMode(gbInfo.isFinalGradeMode());
-		
+				
 		List<CategoryDefinition> newCategoryDefinitions = gbInfo.getCategories();
 		
 		//if we have categories and they are weighted, check the weightings sum up to 100% (or 1 since it's a fraction)
