yongjutianxia/sakai11

CLASSES-2746 Disable "Percentage" Grade Entry Mode and Hide "Grade Entry" accordion menu

gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsGradeEntryPanel.java
@@ -60,7 +60,7 @@ public class SettingsGradeEntryPanel extends Panel {
 		final RadioGroup<Integer> gradeEntry = new RadioGroup<>("gradeEntry",
 				new PropertyModel<Integer>(this.model, "gradebookInformation.gradeType"));
 		gradeEntry.add(new Radio<>("points", Model.of(GbGradingType.POINTS.getValue())));
-		gradeEntry.add(new Radio<>("percentages", Model.of(GbGradingType.PERCENTAGE.getValue())));
+		gradeEntry.add(new Radio<>("percentages", Model.of(GbGradingType.PERCENTAGE.getValue())).setEnabled(false));
 		settingsGradeEntryPanel.add(gradeEntry);
 
 	}

payten/sakai

gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsGradeEntryPanel.java
@@ -55,7 +55,7 @@ public class SettingsGradeEntryPanel extends BasePanel {
 				new PropertyModel<Integer>(this.model, "gradebookInformation.gradeType"));
 
 		gradeEntry.add(new Radio<>("points", Model.of(GradingType.POINTS.getValue())));
-		gradeEntry.add(new Radio<>("percentages", Model.of(GradingType.PERCENTAGE.getValue())));
+		gradeEntry.add(new Radio<>("percentages", Model.of(GradingType.PERCENTAGE.getValue())).setEnabled(false));
 		settingsGradeEntryPanel.add(gradeEntry);
 
 	}