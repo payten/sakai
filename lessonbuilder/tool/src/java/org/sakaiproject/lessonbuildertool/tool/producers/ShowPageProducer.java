4774ea60bb532291769c80ef73d4cd89b4a7e4eb
Payten Giles <payten.giles@gmail.com>
2017-07-11 13:17:37 +1000

    CLASSES-2900 retain user context after submit of a multiple choice or short answer lessons question

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
@@ -1285,6 +1285,9 @@ public class ShowPageProducer implements ViewComponentProducer, DefaultView, Nav
 
 				UIBranchContainer tableRow = UIBranchContainer.make(tableContainer, "item:");
 
+				// NYU we want a consistent way to get the item id for both students and instructors
+				tableRow.decorate(new UIFreeAttributeDecorator("data-itemid", String.valueOf(i.getId())));
+
 				// set class name showing what the type is, so people can do funky CSS
 
 				String itemClassName = null;
@@ -3268,6 +3271,9 @@ public class ShowPageProducer implements ViewComponentProducer, DefaultView, Nav
 			UIOutput.make(tofill, "lessonsSubnavPageId")
 				.decorate(new UIFreeAttributeDecorator("value", String.valueOf(simplePageBean.getCurrentPage().getPageId())));
 		}
+		// NYU set lessonsCurrentPageId
+		UIOutput.make(tofill, "lessonsCurrentPageId")
+			.decorate(new UIFreeAttributeDecorator("value", String.valueOf(simplePageBean.getCurrentPage().getPageId())));
 	}
 	
 	public void makeCsrf(UIContainer tofill, String rsfid) {
