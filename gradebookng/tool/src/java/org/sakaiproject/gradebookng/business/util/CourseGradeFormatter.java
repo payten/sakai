1760d2a344392740fa7d22e354e5aa8cd1224a8b
Payten Giles <payten.giles@gmail.com>
2017-08-03 13:28:00 +1000

    CLASSES-2863 TA should be able to view all course grade elements if they have been granted permission (regardless of whether the course grade is released to students)

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/CourseGradeFormatter.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/CourseGradeFormatter.java
@@ -111,7 +111,7 @@ public class CourseGradeFormatter {
 		}
 
 		if (StringUtils.isNotBlank(letterGrade)
-				&& (this.gradebook.isCourseLetterGradeDisplayed() || this.currentUserRole == GbRole.INSTRUCTOR)) {
+				&& (this.gradebook.isCourseLetterGradeDisplayed() || shouldDisplayFullCourseGrade())) {
 			parts.add(letterGrade);
 		}
 
@@ -133,7 +133,7 @@ public class CourseGradeFormatter {
 		}
 
 		if (StringUtils.isNotBlank(calculatedGrade)
-				&& (this.gradebook.isCourseAverageDisplayed() || this.currentUserRole == GbRole.INSTRUCTOR)) {
+				&& (this.gradebook.isCourseAverageDisplayed() || shouldDisplayFullCourseGrade())) {
 			if (parts.isEmpty()) {
 				parts.add(new StringResourceModel("coursegrade.display.percentage-first", null,
 						new Object[] { calculatedGrade }).getString());
@@ -161,7 +161,7 @@ public class CourseGradeFormatter {
 
 				// if instructor, show the points if requested
 				// otherwise check the settings
-				if (this.currentUserRole == GbRole.INSTRUCTOR || this.gradebook.isCoursePointsDisplayed()) {
+				if (shouldDisplayFullCourseGrade() || this.gradebook.isCoursePointsDisplayed()) {
 					if(pointsEarned != null && totalPointsPossible != null) {
 						if (parts.isEmpty()) {
 							parts.add(MessageHelper.getString("coursegrade.display.points-first", pointsEarned, totalPointsPossible));
@@ -181,4 +181,7 @@ public class CourseGradeFormatter {
 		return String.join(" ", parts);
 	}
 
+	private boolean shouldDisplayFullCourseGrade() {
+		return GbRole.INSTRUCTOR.equals(this.currentUserRole) || GbRole.TA.equals(this.currentUserRole);
+	}
 }
