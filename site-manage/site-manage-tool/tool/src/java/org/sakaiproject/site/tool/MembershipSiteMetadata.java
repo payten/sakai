ec1e337d12264ce5cbc28fa9409b6615439b36f4
Mark Triggs <mark@dishevelled.net>
2017-08-17 17:04:41 +1000

    CLASSES-2965 My Memberships table updates
      * Show the creation date of each site
    
      * Sort sites in descending order of creation adte
    
      * Hide the "Sections" column

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/MembershipSiteMetadata.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/MembershipSiteMetadata.java
@@ -1,6 +1,11 @@
 package org.sakaiproject.site.tool;
 
 import java.util.List;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.TimeZone;
+import org.sakaiproject.time.cover.TimeService;
+import org.sakaiproject.util.ResourceLoader;
 
 import lombok.Data;
 
@@ -16,7 +21,18 @@ public class MembershipSiteMetadata {
 	private String siteId;
 	private String term;
 	private String siteStatus;
-	
+	private Date createdDate;
+
 	private String instructorEids;
 	private List<String> groupSectionEids; //needs to be processed in the vm
+
+	public String getCreatedDate() {
+		ResourceLoader rb = new ResourceLoader("membership");
+
+		final DateFormat df = DateFormat.getDateInstance(DateFormat.DEFAULT, rb.getLocale());
+		final TimeZone tz = TimeService.getLocalTimeZone();
+		df.setTimeZone(tz);
+
+		return df.format(createdDate);
+	}
 }
