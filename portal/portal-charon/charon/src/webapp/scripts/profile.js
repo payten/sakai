655cfead39a67c472c3796fe335c4b15ea21ee47
Payten Giles <payten.giles@gmail.com>
2018-04-03 11:38:25 +1000

    CLASSES-3236 introduce new profile popup and drawer

---
+++ portal/portal-charon/charon/src/webapp/scripts/profile.js
@@ -0,0 +1,333 @@
+function ProfilePopup($link, userUuid, siteId) {
+    this.$link = $PBJQ($link);
+
+    if (userUuid) {
+      this.userUuid = userUuid;
+    } else {
+      this.userUuid = this.$link.data('userUuid') || this.$link.data('useruuid');
+    }
+
+    if (siteId) {
+      this.siteId = siteId;
+    } else { 
+      this.siteId = this.$link.data('siteId') || this.$link.data('siteid');
+    }
+
+    if (!this.userUuid) {
+      throw 'No userUuid provided';
+    }
+};
+
+ProfilePopup.prototype.show = function() {
+    var self = this;
+
+    self.$link.qtip({
+        position: {
+            viewport: $PBJQ(window),
+            at: 'bottom center',
+            adjust: { method: 'flipinvert none'}
+        },
+        show: {
+            ready: true,
+            solo: true,
+            delay: 0
+        },
+        style: {
+            classes: 'qtip-shadow qtip-profile-popup',
+            tip: {
+               corner: true,
+               offset: 20,
+               mimic: 'center',
+               width: 20,
+               height: 8,
+            }
+        },
+        hide: { event: 'click unfocus' },
+        content: {
+            ajax: {
+                method: 'GET',
+                url: "/direct/portal/" + self.userUuid + "/formatted",
+                data: {
+                  siteId: self.siteId,
+                },
+                dataType: 'html',
+                once: false,
+                accepts: {html: 'application/javascript'},
+                cache: false,
+                success: function(data, status) {
+                    this.set('content.text', data);
+                }
+            },
+            text: 'Loading...',
+        },
+        events: {
+            hidden: function(event, api) {
+                self.$link.qtip('destroy', true);
+            },
+            render: function(event, api) {
+                self.addHandlers($(event.target));
+            },
+        }
+    });
+};
+
+ProfilePopup.prototype.addHandlers = function($popup) {
+    var self = this;
+};
+
+ProfilePopup.prototype.rerender = function() {
+    var self = this;
+
+    if (self.$link.data('qtip') && !self.$link.data('qtip').destroyed) {
+        self.$link.qtip('api').destroy();
+    }
+
+    self.show();
+};
+
+
+function ProfileDrawer(userUuid, siteId) {
+    this.userUuid = userUuid;
+    this.siteId = siteId;
+
+    if ($('#profile-drawer').length == 0) {
+        $(document.body).append($("<div id='profile-drawer'>").hide());
+    }
+
+    this.$drawer = $('#profile-drawer');
+
+    if (!this.userUuid) {
+        throw 'No userUuid provided';
+    }
+
+    this.attachEvents();
+};
+
+ProfileDrawer.prototype.show = function() {
+    var self = this;
+
+    $.ajax({
+        method: 'GET',
+        url: "/direct/portal/" + self.userUuid + "/drawer",
+        data: {
+          siteId: self.siteId,
+        },
+        cache: false
+    })
+    .then(function (html) {
+        return self.render(html);
+    }, function (xhr, status, error) {
+        console.error('ProfileDrawer.show', status + ': ' + error);
+    });
+};
+
+ProfileDrawer.prototype.render = function(html) {
+    var self = this;
+
+    self.$drawer.html(html);
+
+    if (!self.$drawer.is(':visible')) {
+        self.$drawer.css('visibility', 'hidden');
+        self.$drawer.show();
+        self.reposition();
+        self.$drawer.css('right', -self.$drawer.width() + 'px');
+        self.$drawer.css('visibility', 'visible');
+        self.$drawer.animate({
+            right: 0
+        }, 500);
+    }
+};
+
+ProfileDrawer.prototype.attachEvents = function() {
+    var self = this;
+
+    function redraw() {
+        if (self.$drawer.is(':visible')) {
+            self.reposition();
+        }
+    };
+    $(document).off('scroll', redraw).on('scroll', redraw);
+    $(window).off('resize', redraw).on('resize', redraw);
+
+
+    self.$drawer
+        .off('click')
+        .on('click', '.profile-connect-button', function(event) {
+            ProfileHelper.requestFriend($(this).data('currentuserid'), $(this).data('connectionuserid'), function(text, status) {
+                self.rerender();
+            });
+        })
+        .on('click', '.profile-accept-button', function(event) {
+            ProfileHelper.confirmFriendRequest($(this).data('currentuserid'), $(this).data('connectionuserid'), function(text, status) {
+                self.rerender();
+            });
+        })
+        .on('click', '.profile-ignore-button', function(event) {
+            ProfileHelper.ignoreFriendRequest($(this).data('currentuserid'), $(this).data('connectionuserid'), function(text, status) {
+                self.rerender();
+            });
+        })
+        .on('click', '.profile-remove-connection-button', function(event) {
+            ProfileHelper.removeFriend($(this).data('currentuserid'), $(this).data('connectionuserid'), function(text, status) {
+                self.rerender();
+            });
+        })
+        .on('click', '.close', function(event) {
+            self.$drawer.animate({
+               right: -self.$drawer.width() + 'px'
+            }, 500, function() {
+               self.$drawer.hide();
+            });
+        });
+};
+
+ProfileDrawer.prototype.rerender = function() {
+    var self = this;
+
+    self.show();
+};
+
+ProfileDrawer.prototype.reposition = function() {
+    var self = this;
+    var offset = $('.Mrphs-mainHeader').height() + $('.Mrphs-mainHeader').offset().top;
+    var topScroll = $(document).scrollTop();
+    if ($(document).scrollTop() < offset) {
+        var magicNumber = offset - topScroll;
+        self.$drawer.css('height', $(window).height() - magicNumber);
+        self.$drawer.css('top', magicNumber);
+    } else {
+        self.$drawer.css('height', '100%');
+        self.$drawer.css('top', '0');
+    }
+};
+
+
+var ProfileHelper = {};
+ProfileHelper.registerPopupLinks = function($container) {
+    if (!$container) {
+        $container = $PBJQ(document.body);
+    }
+
+    function callback(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var pp = new ProfilePopup($PBJQ(this));
+        pp.show();
+    };
+
+    $container.on('click', '.profile-popup[data-userUuid],.profile-popup[data-useruuid]', callback);
+};
+
+ProfileHelper.registerDrawerLinks = function() {
+    function callback(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var pd = new ProfileDrawer($PBJQ(this).data('useruuid'), $PBJQ(this).data('siteid'));
+        pd.show();
+    };
+
+    $PBJQ(document.body).on('click', '.profile-link[data-useruuid]', callback);
+};
+
+ProfileHelper.CONNECTION_NONE = 0;
+ProfileHelper.CONNECTION_REQUESTED = 1;
+ProfileHelper.CONNECTION_INCOMING = 2;
+ProfileHelper.CONNECTION_CONFIRMED = 3;
+
+ProfileHelper.friendStatus = function(requestorId, friendId) {
+    var status = null;
+
+    $PBJQ.ajax({
+        url : "/direct/profile/" + requestorId + "/friendStatus.json?friendId=" + friendId,
+          dataType : "json",
+          async : false,
+      cache: false,
+      success : function(data) {
+          status = data.data;
+      },
+      error : function() {
+          status = -1;
+      }
+    });
+
+    return status;
+};
+
+ProfileHelper.requestFriend = function(requestorId, friendId, callback) {
+
+    if (callback == null) {
+        callback = $PBJQ.noop;
+    }
+
+    jQuery.ajax( {
+        url : "/direct/profile/" + requestorId + "/requestFriend?friendId=" + friendId,
+        dataType : "text",
+        cache: false,
+        success : function(text,status) {
+            callback(text, status);
+        }
+    });
+
+    return false;
+};
+
+ProfileHelper.confirmFriendRequest = function(requestorId, friendId, callback) {
+
+    if (callback == null) {
+        callback = $PBJQ.noop;
+    }
+
+    jQuery.ajax( {
+        url : "/direct/profile/" + requestorId + "/confirmFriendRequest?friendId=" + friendId,
+        dataType : "text",
+        cache: false,
+        success : function(text,status) {
+            callback(text, status);
+        }
+    });
+
+    return false;
+}
+
+ProfileHelper.removeFriend = function(removerId, friendId, callback) {
+
+    if (callback == null) {
+        callback = $PBJQ.noop;
+    }
+
+    jQuery.ajax( {
+        url : "/direct/profile/" + removerId + "/removeFriend?friendId=" + friendId,
+        dataType : "text",
+        cache: false,
+        success : function(text,status) {
+            callback(text, status);
+        }
+    });
+
+    return false;
+};
+
+ProfileHelper.ignoreFriendRequest = function(removerId, friendId, callback) {
+
+    if (callback == null) {
+        callback = $PBJQ.noop;
+    }
+
+    jQuery.ajax( {
+        url : "/direct/profile/" + removerId + "/ignoreFriendRequest?friendId=" + friendId,
+        dataType : "text",
+        cache: false,
+        success : function(text,status) {
+            callback(text, status);
+        }
+    });
+
+    return false;
+}
+
+$PBJQ(document).ready(function() {
+  ProfileHelper.registerPopupLinks();
+  ProfileHelper.registerDrawerLinks();
+});
--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
@@ -2,12 +2,11 @@ package org.sakaiproject.portal.entityprovider;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.Locale;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.velocity.Template;
@@ -16,7 +15,12 @@ import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.apache.velocity.runtime.RuntimeConstants;
 
+import org.sakaiproject.authz.api.Member;
+import org.sakaiproject.authz.api.Role;
 import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.coursemanagement.api.CourseManagementService;
+import org.sakaiproject.coursemanagement.api.EnrollmentSet;
+import org.sakaiproject.coursemanagement.api.Section;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
@@ -30,14 +34,21 @@ import org.sakaiproject.entitybroker.entityprovider.extension.ActionReturn;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.exception.EntityException;
 
-import org.sakaiproject.profile2.logic.ProfileConnectionsLogic;
-import org.sakaiproject.profile2.logic.ProfileLogic;
-import org.sakaiproject.profile2.logic.ProfileLinkLogic;
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.profile2.logic.*;
+import org.sakaiproject.profile2.model.BasicPerson;
+import org.sakaiproject.profile2.model.ProfileImage;
 import org.sakaiproject.profile2.model.UserProfile;
+import org.sakaiproject.profile2.types.PrivacyType;
 import org.sakaiproject.profile2.util.ProfileConstants;
 
+import org.sakaiproject.site.api.Group;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiproject.util.ResourceLoader;
 
 import org.sakaiproject.portal.beans.PortalNotifications;
@@ -65,12 +76,25 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 	private ProfileLinkLogic profileLinkLogic;
 
 	@Setter
+	private ProfilePrivacyLogic privacyLogic;
+
+	@Setter
 	private ServerConfigurationService serverConfigurationService;
 
 	@Setter
 	private SessionManager sessionManager;
 
+	@Setter
+	private UserDirectoryService userDirectoryService;
+
+	@Setter
+	private SiteService siteService;
+
+	@Setter
+	private CourseManagementService cms;
+
 	private Template formattedProfileTemplate = null;
+	private Template profileDrawerTemplate = null;
 
 	public void init() {
 
@@ -83,7 +107,8 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		ve.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogSystem");
 		try {
 			ve.init();
-			formattedProfileTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/profile-popup.vm");
+			formattedProfileTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/nyu-profile-popup.vm");
+			profileDrawerTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/nyu-profile-drawer.vm");
 		} catch (Exception e) {
 			log.error("Failed to load profile-popup.vm", e);
 		}
@@ -121,7 +146,7 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 	}
 
 	@EntityCustomAction(action="formatted",viewKey=EntityView.VIEW_SHOW)
-	public ActionReturn getFormattedProfile(EntityReference ref) {
+	public ActionReturn getFormattedProfile(EntityReference ref, Map<String, Object> params) {
 
 		String currentUserId = developerHelperService.getCurrentUserId();
 
@@ -136,7 +161,7 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		context.put("profileUrl", profileLinkLogic.getInternalDirectUrlToUserProfile(connectionUserId));
 
 		String email = userProfile.getEmail();
-        if (StringUtils.isEmpty(email)) email = "";
+		if (StringUtils.isEmpty(email)) email = "";
 		context.put("email", email);
 
 		context.put("currentUserId", currentUserId);
@@ -148,24 +173,21 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		context.put("acceptLabel", rl.getString("connection.accept"));
 		context.put("rejectLabel", rl.getString("connection.reject"));
 		context.put("addConnectionLabel", rl.getString("connection.add"));
+		context.put("you", currentUserId.equals(connectionUserId));
 
-		boolean connectionsEnabled = serverConfigurationService.getBoolean("profile2.connections.enabled",
-					ProfileConstants.SAKAI_PROP_PROFILE2_CONNECTIONS_ENABLED);
-
-		if (connectionsEnabled && !currentUserId.equals(connectionUserId)) {
-
-			int connectionStatus = profileConnectionsLogic.getConnectionStatus(currentUserId, connectionUserId);
-
-			if (connectionStatus == ProfileConstants.CONNECTION_CONFIRMED) {
-				context.put("connected" , true);
-			} else if (connectionStatus == ProfileConstants.CONNECTION_REQUESTED) {
-				context.put("requested" , true);
-			} else if (connectionStatus == ProfileConstants.CONNECTION_INCOMING) {
-				context.put("incoming" , true);
-			} else {
-				context.put("unconnected" , true);
-			}
+		// NYU things!
+		BasicPerson person = profileLogic.getBasicPerson(connectionUserId);
+		context.put("pictureUrl", "/direct/profile/" + connectionUserId + "/image/thumb");
+		String eid;
+		try {
+			eid = userDirectoryService.getUserEid(connectionUserId);
+			context.put("eid", eid);
+		} catch (UserNotDefinedException e) {
+			// FIXME do something else?
+			throw new RuntimeException("User is not real");
 		}
+		String siteId = (String)params.get("siteId");
+		addSectionAndRoleToContext(context, siteId, connectionUserId);
 
 		StringWriter writer = new StringWriter();
 
@@ -174,6 +196,162 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 			return new ActionReturn(Formats.UTF_8, Formats.HTML_MIME_TYPE, writer.toString());
 		} catch (IOException ioe) {
 			throw new EntityException("Failed to format profile.", ref.getReference());
+		}
+	}
+
+	@EntityCustomAction(action="drawer",viewKey=EntityView.VIEW_SHOW)
+	public ActionReturn getProfileDrawer(EntityReference ref, Map<String, Object> params) {
+
+		String siteId = (String)params.get("siteId");
+
+		String currentUserId = developerHelperService.getCurrentUserId();
+
+		ResourceLoader rl = new ResourceLoader(currentUserId, "profile-popup");
+
+		UserProfile userProfile = (UserProfile) profileLogic.getUserProfile(ref.getId());
+
+		String connectionUserId = userProfile.getUserUuid();
+
+		VelocityContext context = new VelocityContext();
+		context.put("currentUserId", currentUserId);
+		context.put("connectionUserId", connectionUserId);
+		context.put("you", currentUserId.equals(connectionUserId));
+
+		context.put("displayName", userProfile.getDisplayName());
+		context.put("profileUrl", profileLinkLogic.getInternalDirectUrlToUserProfile(connectionUserId));
+		context.put("pictureUrl", "/direct/profile/" + connectionUserId + "/image");
+
+		try {
+			context.put("eid", userDirectoryService.getUserEid(connectionUserId));
+		} catch (UserNotDefinedException e) {
+			context.put("eid", "");
+		}
+
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_BASICINFO)) {
+			boolean showAge = privacyLogic.isBirthYearVisible(connectionUserId);
+			addBasicInfoToContext(context, userProfile, showAge);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_STAFFINFO)) {
+			addStaffInfoToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_STUDENTINFO)) {
+			addStudentInfoToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_PERSONALINFO)) {
+			addBiographyToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_CONTACTINFO)) {
+			addContactInfoToContext(context, userProfile);
+		}
+		addSectionAndRoleToContext(context, siteId, connectionUserId);
+		addConnectionDataToContext(context, currentUserId, connectionUserId);
+
+		StringWriter writer = new StringWriter();
+
+		try {
+			profileDrawerTemplate.merge(context, writer);
+			return new ActionReturn(Formats.UTF_8, Formats.HTML_MIME_TYPE, writer.toString());
+		} catch (IOException ioe) {
+			throw new EntityException("Failed to format profile.", ref.getReference());
+		}
+	}
+
+	private void addSectionAndRoleToContext(VelocityContext context, String siteId, String connectionUserId) {
+		String sections = "";
+		String role = "";
+
+		if (!StringUtils.isBlank(siteId)) {
+			try {
+				Site site = siteService.getSite(siteId);
+				Member member = site.getMember(connectionUserId);
+				if (member != null) {
+					Role memberRole = member.getRole();
+					if (memberRole != null) {
+						role = memberRole.getId();
+					}
+					List<String> groups = new ArrayList<>();
+					Collection<Group> siteGroups = site.getGroups();
+					for (Group group : siteGroups) {
+						if (group.getMember(connectionUserId) != null) {
+							groups.add(group.getTitle());
+						}
+					}
+					sections = groups.stream().collect(Collectors.joining(", "));
+				}
+			} catch (IdUnusedException e) {
+			}
+		}
+
+		context.put("siteId", siteId);
+		context.put("sections", sections);
+		context.put("role", role);
+	}
+
+	private void addConnectionDataToContext(VelocityContext context, String currentUserId, String connectionUserId) {
+		boolean connectionsEnabled = serverConfigurationService.getBoolean("profile2.connections.enabled",
+			ProfileConstants.SAKAI_PROP_PROFILE2_CONNECTIONS_ENABLED);
+
+		if (connectionsEnabled && !currentUserId.equals(connectionUserId)) {
+
+			int connectionStatus = profileConnectionsLogic.getConnectionStatus(currentUserId, connectionUserId);
+
+			if (connectionStatus == ProfileConstants.CONNECTION_CONFIRMED) {
+				context.put("connected", true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_REQUESTED) {
+				context.put("requested", true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_INCOMING) {
+				context.put("incoming", true);
+			} else {
+				context.put("unconnected", true);
+			}
+		}
+	}
+
+	private void addBasicInfoToContext(VelocityContext context, UserProfile profile, boolean showAge) {
+		context.put("showBasicInfo", true);
+		if (StringUtils.isBlank(profile.getNickname())) {
+			context.put("preferredName", profile.getDisplayName());
+		} else {
+			context.put("preferredName", profile.getNickname());
+		}
+
+		if (showAge) {
+			Date dob = profile.getDateOfBirth();
+			if (dob != null) {
+				Calendar birthday = new GregorianCalendar();
+				birthday.setTime(dob);
+				Calendar today = new GregorianCalendar();
+				today.setTime(new Date());
+				int age = today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+
+				context.put("age", String.valueOf(age));
+			}
+		}
+	}
+
+	private void addStaffInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showStaffInfo", true);
+		context.put("position", profile.getPosition());
+		context.put("department", profile.getDepartment());
+	}
+
+	private void addStudentInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showStudentInfo", true);
+		context.put("school", profile.getSchool());
+		context.put("degree", profile.getCourse());
+		context.put("subjects", profile.getSubjects());
+	}
+
+	private void addBiographyToContext(VelocityContext context, UserProfile profile) {
+		context.put("showBiography", true);
+		context.put("personalSummary", profile.getPersonalSummary());
+		context.put("staffProfile", profile.getStaffProfile());
+	}
+
+	private void addContactInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showContactInfo", true);
+		if (!StringUtils.isBlank(profile.getEmail())) {
+			context.put("email", profile.getEmail());
 		}
 	}
 }
