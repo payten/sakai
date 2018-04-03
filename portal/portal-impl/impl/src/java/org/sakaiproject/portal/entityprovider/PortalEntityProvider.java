e6b2743988292e9507271bf2ddebc71427720bb5
Payten Giles <payten.giles@gmail.com>
2018-04-03 11:38:25 +1000

    CLASSES-3236 introduce new profile popup and drawer

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/entityprovider/PortalEntityProvider.java
@@ -17,11 +17,18 @@ package org.sakaiproject.portal.entityprovider;
 
 import java.io.IOException;
 import java.io.StringWriter;
+
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Locale;
-import java.util.Map;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,7 +43,12 @@ import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.apache.velocity.runtime.RuntimeConstants;
 
+import org.sakaiproject.authz.api.Member;
+import org.sakaiproject.authz.api.Role;
 import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.coursemanagement.api.CourseManagementService;
+import org.sakaiproject.coursemanagement.api.EnrollmentSet;
+import org.sakaiproject.coursemanagement.api.Section;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
@@ -48,19 +60,29 @@ import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomActi
 import org.sakaiproject.entitybroker.entityprovider.extension.ActionReturn;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.exception.EntityException;
+
 import org.sakaiproject.memory.api.Cache;
 import org.sakaiproject.memory.api.MemoryService;
 import org.sakaiproject.memory.api.SimpleConfiguration;
 import org.sakaiproject.profile2.logic.ProfileConnectionsLogic;
 import org.sakaiproject.profile2.logic.ProfileLogic;
 import org.sakaiproject.profile2.logic.ProfileLinkLogic;
+import org.sakaiproject.profile2.logic.ProfilePrivacyLogic;
 import org.sakaiproject.profile2.model.BasicConnection;
 import org.sakaiproject.profile2.model.SocialNetworkingInfo;
+
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.profile2.model.BasicPerson;
+import org.sakaiproject.profile2.model.ProfileImage;
 import org.sakaiproject.profile2.model.UserProfile;
+import org.sakaiproject.profile2.types.PrivacyType;
 import org.sakaiproject.profile2.util.ProfileConstants;
+
 import org.sakaiproject.search.api.SearchList;
 import org.sakaiproject.search.api.SearchResult;
 import org.sakaiproject.search.api.SearchService;
+import org.sakaiproject.site.api.Group;
+import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.site.api.SiteService.SelectionType;
 import org.sakaiproject.tool.api.Session;
@@ -91,14 +113,27 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 	private MemoryService memoryService;
 	private SearchService searchService;
 	private SessionManager sessionManager;
-	private SiteService siteService;
 	private ProfileConnectionsLogic profileConnectionsLogic;
 	private ProfileLogic profileLogic;
 	private ProfileLinkLogic profileLinkLogic;
-	private ServerConfigurationService serverConfigurationService;
+
+	@Setter
+	private ProfilePrivacyLogic privacyLogic;
+
+	@Setter
 	private UserDirectoryService userDirectoryService;
 
+	@Setter
+	private ServerConfigurationService serverConfigurationService;
+
+	@Setter
+	private SiteService siteService;
+
+	@Setter
+	private CourseManagementService cms;
+
 	private Template formattedProfileTemplate = null;
+	private Template profileDrawerTemplate = null;
 
 	public void init() {
 
@@ -113,7 +148,8 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 		ve.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogSystem");
 		try {
 			ve.init();
-			formattedProfileTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/profile-popup.vm");
+			formattedProfileTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/nyu-profile-popup.vm");
+			profileDrawerTemplate = ve.getTemplate("org/sakaiproject/portal/entityprovider/nyu-profile-drawer.vm");
 		} catch (Exception e) {
 			log.error("Failed to load profile-popup.vm", e);
 		}
@@ -277,7 +313,6 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 
 		context.put("currentUserId", currentUserId);
 		context.put("connectionUserId", connectionUserId);
-
         boolean connectionsEnabled = serverConfigurationService.getBoolean("profile2.connections.enabled",
             ProfileConstants.SAKAI_PROP_PROFILE2_CONNECTIONS_ENABLED);
 
@@ -295,6 +330,21 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
             }
         }
 
+		// NYU things!
+		context.put("you", currentUserId.equals(connectionUserId));
+		BasicPerson person = profileLogic.getBasicPerson(connectionUserId);
+		context.put("pictureUrl", "/direct/profile/" + connectionUserId + "/image/thumb");
+		String eid;
+		try {
+			eid = userDirectoryService.getUserEid(connectionUserId);
+			context.put("eid", eid);
+		} catch (UserNotDefinedException e) {
+			// FIXME do something else?
+			throw new RuntimeException("User is not real");
+		}
+		String siteId = (String)params.get("siteId");
+		addSectionAndRoleToContext(context, siteId, connectionUserId);
+
 		StringWriter writer = new StringWriter();
 
 		try {
@@ -302,6 +352,162 @@ public class PortalEntityProvider extends AbstractEntityProvider implements Auto
 			return new ActionReturn(Formats.UTF_8, Formats.HTML_MIME_TYPE, writer.toString());
 		} catch (IOException ioe) {
 			throw new EntityException("Failed to format profile.", ref.getReference());
+		}
+	}
+
+	@EntityCustomAction(action="drawer",viewKey=EntityView.VIEW_SHOW)
+	public ActionReturn getProfileDrawer(EntityReference ref, Map<String, Object> params) {
+
+		String siteId = (String)params.get("siteId");
+
+		String currentUserId = developerHelperService.getCurrentUserId();
+
+		ResourceLoader rl = new ResourceLoader(currentUserId, "profile-popup");
+
+		UserProfile userProfile = (UserProfile) profileLogic.getUserProfile(ref.getId());
+
+		String connectionUserId = userProfile.getUserUuid();
+
+		VelocityContext context = new VelocityContext();
+		context.put("currentUserId", currentUserId);
+		context.put("connectionUserId", connectionUserId);
+		context.put("you", currentUserId.equals(connectionUserId));
+
+		context.put("displayName", userProfile.getDisplayName());
+		context.put("profileUrl", profileLinkLogic.getInternalDirectUrlToUserProfile(connectionUserId));
+		context.put("pictureUrl", "/direct/profile/" + connectionUserId + "/image");
+
+		try {
+			context.put("eid", userDirectoryService.getUserEid(connectionUserId));
+		} catch (UserNotDefinedException e) {
+			context.put("eid", "");
+		}
+
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_BASICINFO)) {
+			boolean showAge = privacyLogic.isBirthYearVisible(connectionUserId);
+			addBasicInfoToContext(context, userProfile, showAge);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_STAFFINFO)) {
+			addStaffInfoToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_STUDENTINFO)) {
+			addStudentInfoToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_PERSONALINFO)) {
+			addBiographyToContext(context, userProfile);
+		}
+		if (privacyLogic.isActionAllowed(connectionUserId, currentUserId, PrivacyType.PRIVACY_OPTION_CONTACTINFO)) {
+			addContactInfoToContext(context, userProfile);
+		}
+		addSectionAndRoleToContext(context, siteId, connectionUserId);
+		addConnectionDataToContext(context, currentUserId, connectionUserId);
+
+		StringWriter writer = new StringWriter();
+
+		try {
+			profileDrawerTemplate.merge(context, writer);
+			return new ActionReturn(Formats.UTF_8, Formats.HTML_MIME_TYPE, writer.toString());
+		} catch (IOException ioe) {
+			throw new EntityException("Failed to format profile.", ref.getReference());
+		}
+	}
+
+	private void addSectionAndRoleToContext(VelocityContext context, String siteId, String connectionUserId) {
+		String sections = "";
+		String role = "";
+
+		if (!StringUtils.isBlank(siteId)) {
+			try {
+				Site site = siteService.getSite(siteId);
+				Member member = site.getMember(connectionUserId);
+				if (member != null) {
+					Role memberRole = member.getRole();
+					if (memberRole != null) {
+						role = memberRole.getId();
+					}
+					List<String> groups = new ArrayList<>();
+					Collection<Group> siteGroups = site.getGroups();
+					for (Group group : siteGroups) {
+						if (group.getMember(connectionUserId) != null) {
+							groups.add(group.getTitle());
+						}
+					}
+					sections = groups.stream().collect(Collectors.joining(", "));
+				}
+			} catch (IdUnusedException e) {
+			}
+		}
+
+		context.put("siteId", siteId);
+		context.put("sections", sections);
+		context.put("role", role);
+	}
+
+	private void addConnectionDataToContext(VelocityContext context, String currentUserId, String connectionUserId) {
+		boolean connectionsEnabled = serverConfigurationService.getBoolean("profile2.connections.enabled",
+			ProfileConstants.SAKAI_PROP_PROFILE2_CONNECTIONS_ENABLED);
+
+		if (connectionsEnabled && !currentUserId.equals(connectionUserId)) {
+
+			int connectionStatus = profileConnectionsLogic.getConnectionStatus(currentUserId, connectionUserId);
+
+			if (connectionStatus == ProfileConstants.CONNECTION_CONFIRMED) {
+				context.put("connected", true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_REQUESTED) {
+				context.put("requested", true);
+			} else if (connectionStatus == ProfileConstants.CONNECTION_INCOMING) {
+				context.put("incoming", true);
+			} else {
+				context.put("unconnected", true);
+			}
+		}
+	}
+
+	private void addBasicInfoToContext(VelocityContext context, UserProfile profile, boolean showAge) {
+		context.put("showBasicInfo", true);
+		if (StringUtils.isBlank(profile.getNickname())) {
+			context.put("preferredName", profile.getDisplayName());
+		} else {
+			context.put("preferredName", profile.getNickname());
+		}
+
+		if (showAge) {
+			Date dob = profile.getDateOfBirth();
+			if (dob != null) {
+				Calendar birthday = new GregorianCalendar();
+				birthday.setTime(dob);
+				Calendar today = new GregorianCalendar();
+				today.setTime(new Date());
+				int age = today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+
+				context.put("age", String.valueOf(age));
+			}
+		}
+	}
+
+	private void addStaffInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showStaffInfo", true);
+		context.put("position", profile.getPosition());
+		context.put("department", profile.getDepartment());
+	}
+
+	private void addStudentInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showStudentInfo", true);
+		context.put("school", profile.getSchool());
+		context.put("degree", profile.getCourse());
+		context.put("subjects", profile.getSubjects());
+	}
+
+	private void addBiographyToContext(VelocityContext context, UserProfile profile) {
+		context.put("showBiography", true);
+		context.put("personalSummary", profile.getPersonalSummary());
+		context.put("staffProfile", profile.getStaffProfile());
+	}
+
+	private void addContactInfoToContext(VelocityContext context, UserProfile profile) {
+		context.put("showContactInfo", true);
+		if (!StringUtils.isBlank(profile.getEmail())) {
+			context.put("email", profile.getEmail());
 		}
 	}
 
