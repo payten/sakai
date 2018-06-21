90fefce48405cffae50ca72ea6f088b5e5573bed
Mark Triggs <mark@dishevelled.net>
2018-06-21 10:56:39 +1000

    Revert "CLASSES-3269 SAM-3290 - Regression from SAM-3254 - feedback link does not work for students (#4920)"
    This reverts commit 84db512d881371eaae9d611befc823d3d220e3e1.

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -157,7 +157,7 @@ public class DeliveryActionListener
       	return;
       }
 
-      if (delivery.pastDueDate() && (DeliveryBean.TAKE_ASSESSMENT == action || DeliveryBean.TAKE_ASSESSMENT_VIA_URL == action)) {
+      if (delivery.pastDueDate()){
         if (delivery.isAcceptLateSubmission()) {
           if(delivery.getTotalSubmissions() > 0) {
             // Not during a Retake
