3104562d7c9b9391ddaeb450181b643a27848812
Payten Giles <payten.giles@gmail.com>
2018-07-12 14:50:42 +1000

    Protect a sheet, clear it, set some values, unprotect it

--- attendance_report/src/java/edu/nyu/classes/servlet/AttendanceGoogleReportExport.java
+++ attendance_report/src/java/edu/nyu/classes/servlet/AttendanceGoogleReportExport.java
@@ -1,9 +1,7 @@
 package edu.nyu.classes.servlet;
 
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.Spreadsheet;
-import com.google.api.services.sheets.v4.model.Sheet;
-import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.services.sheets.v4.model.*;
 
 import org.sakaiproject.component.cover.HotReloadConfigurationService;
 import org.sakaiproject.component.cover.ServerConfigurationService;
@@ -41,6 +39,7 @@ public class AttendanceGoogleReportExport {
             GoogleClient client = new GoogleClient(oauthProperties);
             Sheets service = client.getSheets(APPLICATION_NAME);
 
+            System.out.println("Get the sheet");
             List<String> ranges = new ArrayList<>();
             Sheets.Spreadsheets.Get request = service.spreadsheets().get(spreadsheetId);
             request.setRanges(ranges);
@@ -49,27 +48,79 @@ public class AttendanceGoogleReportExport {
 
             Sheet sheet = spreadsheet.getSheets().get(0);
             String sheetName = sheet.getProperties().getTitle();
+//            String sheetId = sheet.getProperties().getId();
             int columns = sheet.getProperties().getGridProperties().getColumnCount();
-            int rows = sheet.getProperties().getGridProperties().getRowCount();
+            int rowsCount = sheet.getProperties().getGridProperties().getRowCount();
 
             System.out.println("Spreadsheet title: " + sheetName);
             System.out.println("Spreadsheet total columns: " + columns);
-            System.out.println("Spreadsheet total rows: " + rows);
+            System.out.println("Spreadsheet total rows: " + rowsCount);
+            System.out.println(spreadsheet);
 
-            ValueRange response = service.spreadsheets().values()
-                .get(spreadsheetId, sheetName)
-                .execute();
+            System.out.println("Protect it");
+            AddProtectedRangeRequest addProtectedRangeRequest = new AddProtectedRangeRequest();
+            ProtectedRange protectedRange = new ProtectedRange();
+            GridRange gridRange = new GridRange();
+            gridRange.setSheetId(0);
+            protectedRange.setRange(gridRange);
+            protectedRange.setEditors(new Editors());
+            protectedRange.setRequestingUserCanEdit(true);
+            addProtectedRangeRequest.setProtectedRange(protectedRange);
 
-            List<List<Object>> values = response.getValues();
+            BatchUpdateSpreadsheetRequest batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
+            List<Request> requests = new ArrayList<>();
+            Request wrapperRequest = new Request();
+            wrapperRequest.setAddProtectedRange(addProtectedRangeRequest);
+            requests.add(wrapperRequest);
+            batchUpdateSpreadsheetRequest.setRequests(requests);
+            Sheets.Spreadsheets.BatchUpdate batchUpdateRequest =
+                service.spreadsheets().batchUpdate(spreadsheetId, batchUpdateSpreadsheetRequest);
 
-            if (values == null || values.isEmpty()) {
-                System.out.println("No data found.");
-            } else {
-                System.out.println("Testing");
-                for (List row : values) {
-                    System.out.printf("%s %s %s %s %s\n", row.get(0), row.get(1), row.get(2), row.get(3), row.get(4));
+            BatchUpdateSpreadsheetResponse batchUpdateSpreadsheetResponse = batchUpdateRequest.execute();
+            System.out.println(batchUpdateSpreadsheetResponse);
+
+            System.out.println("Clearing the sheet");
+            Sheets.Spreadsheets.Values.Clear clearRequest =
+                service.spreadsheets().values().clear(spreadsheetId, sheetName, new ClearValuesRequest());
+            ClearValuesResponse clearValuesResponse = clearRequest.execute();
+            System.out.println(clearValuesResponse);
+
+            System.out.println("Give it some values");
+            ValueRange valueRange = new ValueRange();
+            List<List<Object>> rows = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                List<Object> row = new ArrayList<>();
+                for (int j = 0; j < 100; j++) {
+                    row.add(String.valueOf(i*j*100));
                 }
+                rows.add(row);
             }
+
+            valueRange.setValues(rows);
+
+            Sheets.Spreadsheets.Values.Update updateRequest =
+                service.spreadsheets().values().update(spreadsheetId, sheetName, valueRange);
+            updateRequest.setValueInputOption("RAW");
+            UpdateValuesResponse updateValuesResponse = updateRequest.execute();
+            System.out.println(updateValuesResponse);
+
+            System.out.println("Unprotect it");
+            requests = new ArrayList<>();
+            Sheets.Spreadsheets.Get getSpreadsheetRequest = service.spreadsheets().get(spreadsheetId);
+            Spreadsheet s = getSpreadsheetRequest.execute();
+            for (ProtectedRange p : s.getSheets().get(0).getProtectedRanges()) {
+                DeleteProtectedRangeRequest deleteProtectedRangeRequest = new DeleteProtectedRangeRequest();
+                deleteProtectedRangeRequest.setProtectedRangeId(p.getProtectedRangeId());
+                wrapperRequest = new Request();
+                wrapperRequest.setDeleteProtectedRange(deleteProtectedRangeRequest);
+                requests.add(wrapperRequest);
+            }
+            batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
+            batchUpdateSpreadsheetRequest.setRequests(requests);
+            batchUpdateRequest = service.spreadsheets().batchUpdate(spreadsheetId, batchUpdateSpreadsheetRequest);
+            batchUpdateSpreadsheetResponse = batchUpdateRequest.execute();
+            System.out.println(batchUpdateSpreadsheetResponse);
         } catch (Exception e) {
             System.out.println("ERROR in AttendanceGoogleReportExport.export");
             System.out.println(e.getMessage());
