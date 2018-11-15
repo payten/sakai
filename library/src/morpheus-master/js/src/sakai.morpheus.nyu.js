06a4c783346fd8fa593cffdfda02b9dfb3d355ff
Payten Giles <payten.giles@gmail.com>
2018-11-15 12:30:51 +1100

    CLASSES-2317 UPGRADE REDO announcement, calendar and chat synoptic tool changes

--- library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
@@ -23,6 +23,21 @@ $(function() {
   }
 });
 
+
+// Introduce Option button for calendar and messages synoptic tool
+// that invokes option link within the tool
+$(function() {
+  $("#synopticCalendarOptions").on("click", function() {
+    var $portlet = $(this).closest(".Mrphs-container.Mrphs-sakai-summary-calendar");
+    var $iframe = $(".Mrphs-toolBody.Mrphs-toolBody--sakai-summary-calendar iframe", $portlet);
+    var $iframeBody = $($iframe[0].contentWindow.document.body);
+
+    // trigger the options link
+    $("#calendarForm .actionToolbar .firstToolBarItem a", $iframeBody).trigger("click");
+  });
+});
+
+
 // Bind a dummy touch event on document to stop iOS from capturing
 // a click to enable a hover state
 $PBJQ(document).on("touchstart", function() { return true; });
