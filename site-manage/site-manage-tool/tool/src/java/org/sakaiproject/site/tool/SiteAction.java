yongjutianxia/sakai11

CLASSES-2095 Fix NPE caused by findTools returning inconsistent tool lists
Parts of SiteAction.java repeatedly call findTools from different
contexts and blow up if it returns different tools at different times.
Add a new "showAllTools" to our FindContext to make sure we're not
returning a subset of tools at the wrong time.

site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -7796,7 +7796,11 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 		}
 		else
 		{
-			Set toolRegistrationList = ToolManager.findTools(Collections.singleton(state.getAttribute(STATE_SITE_TYPE)), null, null);
+                        // This call can return all tools since we're not
+                        // exposing the list to the user anyway.  Otherwise we
+                        // risk the site context saying a tool is unstealthed
+                        // but this call not returning the tool.
+			Set toolRegistrationList = ToolManager.findTools(Collections.singleton(state.getAttribute(STATE_SITE_TYPE)), null, new FindToolsContext().showAllTools());
 			Set categories = new HashSet();
 			categories.add((String) state.getAttribute(STATE_SITE_TYPE));
 			String rv = null;
@@ -11687,7 +11691,7 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 	 * SAK 23808
 	 */
 	private boolean isSiteTypeInToolCategory(String siteType, Tool tool) {
-		Set<Tool> tools = ToolManager.findTools(Collections.emptySet(), null, null);
+		Set<Tool> tools = ToolManager.findTools(Collections.emptySet(), null, new FindToolsContext().showAllTools());
 		Set<String> categories = tool.getCategories();
 		Iterator<String> iterator = categories.iterator();
 		while(iterator.hasNext()) {

payten/sakai

site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -7903,7 +7903,11 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 		}
 		else
 		{
-			Set toolRegistrationSet = ToolManager.findTools(Collections.singleton(state.getAttribute(STATE_SITE_TYPE)), null, null);
+                        // This call can return all tools since we're not
+                        // exposing the list to the user anyway.  Otherwise we
+                        // risk the site context saying a tool is unstealthed
+                        // but this call not returning the tool.
+			Set toolRegistrationSet = ToolManager.findTools(Collections.singleton(state.getAttribute(STATE_SITE_TYPE)), null, new FindToolsContext().showAllTools());
 			Set categories = new HashSet();
 			categories.add((String) state.getAttribute(STATE_SITE_TYPE));
 			String rv = null;
@@ -11642,7 +11646,7 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 	 * SAK 23808
 	 */
 	private boolean isSiteTypeInToolCategory(String siteType, Tool tool) {
-		Set<Tool> tools = ToolManager.findTools(Collections.emptySet(), null, null);
+		Set<Tool> tools = ToolManager.findTools(Collections.emptySet(), null, new FindToolsContext().showAllTools());
 		Set<String> categories = tool.getCategories();
 		Iterator<String> iterator = categories.iterator();
 		while(iterator.hasNext()) {