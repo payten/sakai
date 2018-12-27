8047b3257fbd4a73b491c29153ea3f38bc0600ea
Payten Giles <payten.giles@gmail.com>
2018-12-28 06:24:56 +1100

    CLASSES-3565 suppress PA System popups on mobile

--- pasystem/pasystem-tool/tool/src/webapp/scripts/pasystem.js
+++ pasystem/pasystem-tool/tool/src/webapp/scripts/pasystem.js
@@ -180,6 +180,12 @@ PASystemPopup.prototype.showPopup = function() {
                    },
                    beforeOpen: function() {
                      this.$instance.find(".featherlight-content").attr("tabindex", 0);
+                     // CLASSES-3565 NYU add CSS class to outer featherlight container
+                     if (self.uuid == "preview") {
+                       this.$instance.addClass("pasystem-popup-preview");
+                     } else {
+                       this.$instance.addClass("pasystem-popup");
+                     }
                    },
                    afterOpen: function() {
                      this.$instance.find(".featherlight-content").focus();
