8c6971f44df041f4f259e4e05b71f6d3704fceb0
Payten Giles <payten.giles@gmail.com>
2019-01-21 11:05:27 +1100

    CLASSES-3593 re-enable border table property in CKEditor

--- library/src/webapp-filtered/editor/ckeditor.launch.js
+++ library/src/webapp-filtered/editor/ckeditor.launch.js
@@ -156,7 +156,7 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config, w, h) {
                 classes: true
             }
         },
-        disallowedContent: 'table[cellspacing,cellpadding,border]',
+        disallowedContent: 'table[cellspacing,cellpadding]',
 
         language: language + (country ? '-' + country.toLowerCase() : ''),
         // This is used for uploading by the autorecorder plugin.
