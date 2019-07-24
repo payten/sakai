0003d8fc627828cfa591d258fa112ba4088c71e7
Mark Triggs <mark@dishevelled.net>
2019-07-24 10:07:42 +1000

    CLASSES-3506 Sort descending by created ON, not created BY

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/MembershipAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/MembershipAction.java
@@ -200,11 +200,11 @@ public class MembershipAction extends PagedResourceActionII
 		{
 			if (sortAsc)
 			{
-				rv = SITE_SERV.getSites(SelectionType.MEMBER, null, search, null, SortType.CREATED_BY_ASC, page);
+				rv = SITE_SERV.getSites(SelectionType.MEMBER, null, search, null, SortType.CREATED_ON_ASC, page);
 			}
 			else
 			{
-				rv = SITE_SERV.getSites(SelectionType.MEMBER, null, search, null, SortType.CREATED_BY_DESC, page);
+				rv = SITE_SERV.getSites(SelectionType.MEMBER, null, search, null, SortType.CREATED_ON_DESC, page);
 			}
 		}
 
